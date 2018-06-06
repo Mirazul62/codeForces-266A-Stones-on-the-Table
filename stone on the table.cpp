819426cbdacfaab2c4781571e8470e4e4e6c041a
Mirazul Islam <mirazkuetcse62@gmail.com>
2018-06-07 00:37:48 +0600

    codeForces/266A-stone on the table solve in c++

---
+++ stone on the table.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, n,count=0;
+    string s;
+    cin>>n;
+    cin>>s;
+    for(i=0;i<n-1;i++)
+    {
+        if(s[i]==s[i+1])
+            count++;
+    }
+    cout<<count<<endl;
+}
